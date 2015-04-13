0b6f2d245ec0913cdee8b6fe3577aa383bd821ab
Mark Berrill <mbt@billmp1.ornl.gov>
2015-04-13 13:10:35 -0400

    Fixing cppcheck warnings

--- src/test/test_MemoryApp.cpp
+++ src/test/test_MemoryApp.cpp
@@ -10,11 +10,20 @@
 }while(0)
 
 
+inline void check_ptr( double *tmp ) 
+{
+    std::cout << tmp << std::endl;
+    tmp[0] = rand();
+    if ( tmp[0]==0 )
+        std::cout << "Error in test" << std::endl;
+}
+
 #define ASSERT(EXP) do {                                \
     if ( !(EXP) ) {                                     \
-           std::stringstream tboxos;                    \
-           tboxos << "Failed assertion: " << #EXP << std::ends; \
-           throw std::logic_error(tboxos.str());        \
+        std::stringstream tboxos;                       \
+        tboxos << "Failed assertion: " << #EXP << std::ends; \
+        delete [] tmp;                                  \
+        throw std::logic_error(tboxos.str());           \
     }                                                   \
 }while(0)
 
@@ -35,17 +44,14 @@ int main(int, char*[])
         std::cout << "Skipping new/delete tests (disabled)" << std::endl;
     #else
         double *tmp = new double();
+        check_ptr(tmp);
         MemoryApp::MemoryStats m2 = MemoryApp::getMemoryStats();
-        std::cout << tmp << std::endl;
-        NULL_USE(tmp); ASSERT(tmp>0);
-        tmp[0] = rand(); ASSERT(tmp[0]!=0);
-        delete tmp; tmp = NULL;
+        delete tmp;
         MemoryApp::MemoryStats m3 = MemoryApp::getMemoryStats();
         tmp = new double[1000];
-        std::cout << tmp << std::endl;
-        NULL_USE(tmp); ASSERT(tmp>0);
+        check_ptr(tmp);
         MemoryApp::MemoryStats m4 = MemoryApp::getMemoryStats();
-        delete tmp; tmp = NULL;
+        delete tmp;
         MemoryApp::MemoryStats m5 = MemoryApp::getMemoryStats();
         if ( m2.bytes_new<m1.bytes_new+8 || m2.N_new!=1 ) {
             std::cout << "Failed new test\n";
