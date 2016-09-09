a0cb6a9a99faf55538ddceef774c98bec86f547d
Mark Berrill <mbt@mbt01.ornl.gov>
2016-09-09 10:50:41 -0400

    Minor update to C++ flags

--- src/cmake/macros.cmake
+++ src/cmake/macros.cmake
@@ -373,7 +373,7 @@ MACRO( SET_WARNINGS )
   ELSEIF ( USING_CLANG )
     # Add default compiler options
     SET(CMAKE_C_FLAGS     "${CMAKE_C_FLAGS} -Wall")
-    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-missing-braces -Wno-mismatched-new-delete")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-mismatched-new-delete")
   ELSEIF ( USING_DEFAULT )
     # Add default compiler options
     SET(CMAKE_C_FLAGS     "${CMAKE_C_FLAGS}")
