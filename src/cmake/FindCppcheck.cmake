ab5617ef326463bd061b7d97d14e9896de9fcc1d
Mark Berrill <mbt@mbt01.ornl.gov>
2016-10-03 11:06:04 -0400

    Fixing bug with install

--- src/cmake/FindCppcheck.cmake
+++ src/cmake/FindCppcheck.cmake
@@ -1,14 +1,45 @@
+# FindCppcheck
+# ---------
+#
+# Find cppcheck
+#
+# Use this module by invoking find_package with the form:
+#
+#   find_package( Cppcheck
+#     [REQUIRED]             # Fail with error if the cppcheck is not found
+#   )
+#
+# This module finds cppcheck and configures a test using the provided options
+#
+# This program reconizes the following options
+#   CPPCHECK_INCLUDE      - List of include folders
+#   CPPCHECK_OPTIONS      - List of cppcheck options
+#   CPPCHECK_SOURCE       - Source path to check
+#
+# The following variables are set by find_package( Cppcheck )
+#
+#   CPPCHECK_FOUND        - True if cppcheck was found
+
+
 # Find cppcheck if availible
 FIND_PROGRAM( CPPCHECK 
     NAMES cppcheck cppcheck.exe 
     PATHS "${CPPCHECK_DIRECTORY}" "C:/Program Files/Cppcheck" "C:/Program Files (x86)/Cppcheck" 
 )
 IF ( CPPCHECK )
+    SET( CPPCHECK_FOUND TRUE )
+ELSE()
+    SET( CPPCHECK_FOUND FALSE )
+ENDIF()
+IF ( CPPCHECK_FOUND )
     EXECUTE_PROCESS( COMMAND ${CPPCHECK} --version OUTPUT_VARIABLE CPPCHECK_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE )
     MESSAGE( STATUS "Using cppcheck: ${CPPCHECK_VERSION}")
+ELSEIF ( CPPCHECK_FIND_REQUIRED )
+    MESSAGE( FATAL_ERROR "cppcheck not found")
 ELSE()
     MESSAGE( STATUS "cppcheck not found")
 ENDIF()
+
 
 # Set the options for cppcheck
 IF ( NOT DEFINED CPPCHECK_OPTIONS )
@@ -22,6 +53,7 @@ IF( NOT DEFINED CPPCHECK_INCLUDE )
         SET( CPPCHECK_INCLUDE ${CPPCHECK_INCLUDE} "-I${dir}" )
     ENDFOREACH()
 ENDIF()
+
 
 # Add the test
 IF ( CPPCHECK )
